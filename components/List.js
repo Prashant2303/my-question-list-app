8891f8be13d02c62117e095de896c4903dfac06d
Prashant Kumar <prashant.kumar.3@team.telstra.com>
2022-05-10 18:27:34 +0530

    Added POST API, Added CircularProgress and Removed unused css

--- components/List.js
+++ components/List.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Grid, Paper } from '@mui/material';
+import { CircularProgress, Grid, Paper } from '@mui/material';
 import Question from './Question';
 
-const List = ({ questions }) => {
+const List = ({ questions, loading }) => {
     return (
         <Paper className="form" elevation={3} sx={{ padding: '15px', marginTop: '15px' }}>
             <Grid container spacing={1} alignItems="center" marginBottom="10px">
@@ -11,11 +11,10 @@ const List = ({ questions }) => {
                 <Grid item xs={2}>Status</Grid>
                 <Grid item xs={2}>Action</Grid>
             </Grid>
-            {/* <Grid container spacing={1}>    Adding xs and spacing together causes component to shift left */}
             {
-                questions.map((question) => <Question key={question.id} question={question} />)
+                loading ? <div style={{ 'display': 'flex', 'justifyContent': 'center' }}> <CircularProgress /> </div> :
+                    questions.map((question) => <Question key={question.id} question={question} />)
             }
-            {/* </Grid> */}
         </Paper>
     )
 }
--- pages/api/questions.js
+++ pages/api/questions.js
@@ -9,17 +9,8 @@ export default function handler(req, res) {
         };
         case 'POST': {
             const { question } = req.body;  //get question from req
-            const newQuestion = {           //create question body
-                id: Date.now(),
-                url: 'https://leetcode.com/problems/two-sum/',
-                site: 'Leetcode',
-                name: question,
-                difficulty: 'Easy',
-                status: 'Done',
-                notes: '',
-            }
-            questions.unshift(newQuestion); //add question at beginning of array
-            res.status(200).json(questions);//return new Questions array
+            questions.unshift(question);    //add question at beginning of array
+            res.status(200).json(question);//return question
             break;
         }
     }
