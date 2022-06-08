c5cee2c7fcb9646d95d63758a6ab93cedc5cd16d
Prashant Kumar <prashant.kumar.3@team.telstra.com>
2022-06-08 21:43:08 +0530

    Updated about page text

--- pages/about.js
+++ pages/about.js
@@ -7,8 +7,12 @@ export default function About() {
             <Paper className="form" elevation={3} sx={{ padding: '15px', marginTop: '15px' }}>
                 <h1>About</h1>
                 <div>
-                    MyQuestionList is your personal question diary where you can add links to questions you want and set its difficulty and status according to you.
-                    You can also add/update notes for any question. All this can be achieved using a simple and easy to use interface with the fewest clicks possible.
+                    MyQuestionList is your personal notebook for coding questions where users can create an account add URLs to any question you want
+                    and set its Difficulty, Status and Category according to you. Users can also add/update notes for any question.
+                    It features filter and search functionality as well. All this can be achieved using a simple and easy to use 
+                    interface with the fewest clicks possible. <br />
+                    Built using :- Next.js, Recoil, MUI and MongoDB <br />
+                    Deployed on :- Vercel
                 </div>
             </Paper>
         </Container>
