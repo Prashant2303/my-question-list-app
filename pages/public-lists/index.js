5516a4d24d040f8e3c613c41f8935e603987c8db
Prashant Kumar <prashant.kumar.3@team.telstra.com>
2022-07-04 03:35:23 +0530

    Added feature to search public lists

--- pages/public-lists/index.js
+++ pages/public-lists/index.js
@@ -1,7 +1,9 @@
 import styles from 'styles/PublicLists.module.css';
-import { Paper, Grid, Typography } from "@mui/material";
-import { connectToDatabase } from 'helpers/db';
+import { Paper, Grid, Typography, TextField, Button } from "@mui/material";
+import Fuse from 'fuse.js';
 import Link from 'next/link';
+import { connectToDatabase } from 'helpers/db';
+import { useState } from 'react';
 
 export default function PublicLists({ data }) {
 
@@ -18,21 +20,75 @@ export default function PublicLists({ data }) {
         </Grid>
     )
 
+    const [query, setQuery] = useState(false);
+    const [filterState, setFilterState] = useState({
+        filter: false,
+        filteredQuestions: null
+    });
+
+    const handleQuery = (e) => setQuery(e.target.value);
+
+    const handleKeyDown = (e) => {
+        if (e.keyCode === 13) {
+            const listToQuery = filterState.filter ? filterState.filteredQuestions : data;
+            const fuse = new Fuse(listToQuery, {
+                keys: ['name', 'ownerName']
+            })
+            const result = fuse.search(query);
+            const filtered = result.map(item => item.item);
+            setFilterState({
+                filter: true,
+                filteredQuestions: filtered
+            })
+        }
+    }
+
+    const handleReset = () => {
+        setQuery('');
+        setFilterState({
+            filter: false,
+            filteredQuestions: null
+        });
+    }
+
+    const renderQuestions = filterState.filter ? filterState.filteredQuestions : data;
+
     return (
         <Paper className={styles.container} elevation={3}>
             <Typography variant="h6" component="div">
                 Public Lists
             </Typography>
-            {!data || data?.length === 0 ?
+            <Grid container>
+                <Grid item xs={10} sm={7.3}>
+                    <TextField
+                        name='query'
+                        variant='outlined'
+                        size='small'
+                        label='Search'
+                        fullWidth
+                        value={query}
+                        onChange={handleQuery}
+                        InputProps={{ onKeyDown: handleKeyDown }}
+                    />
+                </Grid>
+                <Grid container item xs={2} sm={2}>
+                    <Button
+                        size="small"
+                        variant="text"
+                        fullWidth
+                        onClick={handleReset}
+                    >
+                        Reset
+                    </Button>
+                </Grid>
+            </Grid>
+            <Grid container className={styles.rowHeadings}>
+                <Grid item xs={8}>Title</Grid>
+                <Grid item xs={4}>Author</Grid>
+            </Grid>
+            {!renderQuestions || renderQuestions?.length === 0 ?
                 <div className={styles.emptyList}>No Public Lists</div> :
-                <>
-                    <Grid container className={styles.rowHeadings}>
-                        <Grid item xs={8}>Title</Grid>
-                        <Grid item xs={4}>Author</Grid>
-                    </Grid>
-                    {data.map(list => ListItem(list))}
-                </>
-            }
+                renderQuestions.map(list => ListItem(list))}
         </Paper>
     )
 }
